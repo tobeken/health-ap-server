953466842a1e3eae3a8583a17b34604cfcc1e7ec
tobeken <tobik0124@gmail.com>
2022-11-03 12:25:54 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+require('dotenv').config()
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const userRoute = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorsRoute")
+
+
+
+app.use('/api/user', userRoute);
+app.use('/api/admin', adminRoute);
+app.use('/api/doctor', doctorRoute);
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port,() => console.log(`Node server started at port ${port}`));
